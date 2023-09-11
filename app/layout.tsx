6860609fa8faae42dd0391c1d4dd16ac817fe957
SomarGuy <diogo.ramos@me.com>
2023-09-11 16:06:57 -0400

    create new folder components & new file Sidebar

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {/* Sidebar */}
+        
+        <main>
+          {/* Header */}
+          {children}
+          
+        </main>
+        </body>
     </html>
   )
 }
