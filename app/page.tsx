b0b4894da783765cd4f850336566219ef45bbf67
SomarGuy <diogo.ramos@me.com>
2023-09-11 16:03:09 -0400

    center logo & text

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,13 @@ import { DocumentMagnifyingGlassIcon } from "@heroicons/react/24/outline"
 
 function HomePage() {
   return (
-    <div>
-        <div>
-            <DocumentMagnifyingGlassIcon className="h-64 w-64 text-cyan-600/20" />
+        <div className="flex flex-col items-center justify-center">
+            <DocumentMagnifyingGlassIcon className="h-64 w-64 text-cyan-600/20" 
+            />
+            <h1 className="text-3xl text-center mt-2 text-black font-bold mb-5">
+                Welcome to the Amazon Web Scraper
+            </h1>
         </div>
-    </div>
   )
 }
 
