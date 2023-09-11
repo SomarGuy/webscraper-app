6b10b3fceed3bef3d8402e5858c7f3538629a768
SomarGuy <diogo.ramos@me.com>
2023-09-11 16:15:37 -0400

    add styling to inner and outer div

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -2,16 +2,24 @@ import { MagnifyingGlassCircleIcon } from "@heroicons/react/24/outline"
 
 function Sidebar() {
   return (
-    <div>
-        <div>
+    <div className="p-2 md:p-10 py-6 overflow-y-auto border-b border-cyan-500/50
+    ">
+        <div className="flex flex-col items-center justify-center
+        mb-10">
         <MagnifyingGlassCircleIcon className="h-16 md:w-16 text-cyan-600" />
 
         <h1 className="hidden md:inline text-3xl mt-2">Web Scraper</h1>
-        <h2>Scraping the Web, One Byte at a Time!</h2>
+        <h2 className="hidden md:inline text-xs italic">Scraping the Web, One Byte at a Time!</h2>
         </div>
 
         <ul>
-
+            {/* SidebarRow */}
+            {/* SidebarRow */}
+            {/* SidebarRow */}
+            {/* SidebarRow */}
+            {/* SidebarRow */}
+            {/* SidebarRow */}
+            {/* SidebarRow */}
         </ul>
     </div>
   )
